e3bcbeee9515ea9b019eeef2a8c8dead32f0d3d1
getMaxed <maxfaden07@gmail.com>
2025-08-05 08:10:52 -0700

    xzcvxcv

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,37 @@
 import React from 'react'
 
+const DEFAULT_TEMPO = 140
+const DEFAULT_DUR = 8
+const DEFAULT_METRONOME_FREQ = 4
+
 export function App() {
-	const input1Ref = React.useRef<HTMLInputElement>(null)
+	// Tempo
+	const [tempo, setTempo] = React.useState(DEFAULT_TEMPO)
+	const tempoInputRef = React.useRef<HTMLInputElement>(null)
+
+	// Metronome
+	const [isMetronomeOn, setIsMetronomeOn] = React.useState(false)
+	const [metronomeFreq, setMetronomeFreq] = React.useState(DEFAULT_METRONOME_FREQ)
+
+	
+
+	
+	const [dur, setDur] = React.useState(DEFAULT_DUR)
+
 
 	React.useEffect(() => {
-		input1Ref.current?.focus()
-
 		const keyboardEventHandler = (e: KeyboardEvent) => {
-			if (e.ctrlKey && e.key === "a") {
-				alert("ctrl+a")
+
+		switch (e.key) {
+			case "/":
+				if (document.activeElement === tempoInputRef.current) {
+					tempoInputRef?.current?.blur();
+				} else {
+					tempoInputRef.current?.focus();
+				}
+				break;
 			}
-		}
+		};
 
 		window.addEventListener("keydown", keyboardEventHandler)
 		return () => window.removeEventListener("keydown", keyboardEventHandler)
@@ -19,14 +40,21 @@ export function App() {
 	return (
 		<>
 			{/* Tempo */}
+			{/* <div style={{ marginBottom: 12 }}>
+				<label htmlFor="tempo" style={{ marginRight: 12 }}>Tempo</label>
+				<p>HK Mode: {isHotKeyMode.toString()}</p>
+			</div> */}
+
+			{/* Tempo */}
 			<div style={{ marginBottom: 12 }}>
 				<label htmlFor="tempo" style={{ marginRight: 12 }}>Tempo</label>
-				<input ref={input1Ref} type="text" id="tempo" />
+				<input value={tempo} step={10} onChange={e => setTempo(Number(e.target.value))} type="number" ref={tempoInputRef} id="tempo" />
 			</div>
 
-			<div style={{ marginBottom: 12 }}>
-				<input type="text" />
-			</div>
+			{/* <div style={{ marginBottom: 12 }}>
+				<label htmlFor="dur" style={{ marginRight: 12 }}>Duration</label>
+				<input value={dur} type="text" id="dur" />
+			</div> */}
 
 			<div style={{ marginBottom: 12 }}>
 				<input type="text" />
@@ -40,3 +68,28 @@ export function App() {
 }
 
 export default App
+
+
+			// case "a":
+			// 	setIsMetronomeOn(m => !m);
+			// 	break;
+			// case "w":
+			// 	setDur(prev => {
+			// 		const i = DURATION_STEPS.indexOf(prev);
+			// 		return DURATION_STEPS[(i + 1) % DURATION_STEPS.length];
+			// 	});
+			// 	break;
+			// case "s":
+			// 	setDur(prev => {
+			// 		const i = DURATION_STEPS.indexOf(prev);
+			// 		return DURATION_STEPS[(i - 1 + DURATION_STEPS.length) % DURATION_STEPS.length];
+			// 	});
+			// 	break;
+
+
+			// const [isHotKeyMode, setIsHotKeyMode] = React.useState(false)
+
+				// if (e.altKey && e.shiftKey) {
+	// if (e.ctrlKey) {
+		// e.preventDefault();
+		// e.stopPropagation();
